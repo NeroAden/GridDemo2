b69ec8956ed80edcbd35e931e4108b5555c28271
neroaden <neroaden0515@outlook.com>
2018-04-18 19:15:10 +0800

    添加注释

--- GridDemo2/Form1.cs
+++ GridDemo2/Form1.cs
@@ -12,10 +12,26 @@ namespace GridDemo2
 {
     public partial class Form1 : Form
     {
+        /*
+         * 初始化程序
+         * 实例化Map--背景网格
+         * People--贪吃蛇对象
+         * Crah--果子对象
+         * backGround--网格图层
+         * boll--贪吃蛇图层
+         * item--果子图层
+         * timer--总定时器
+         * way--蛇头朝向
+         * init()--初始化相关设置
+         * Timer_Tick(object sender, EventArgs e)--定时器定时事件（移动蛇以及相关逻辑）
+         * button1_Click(object sender, EventArgs e)--button1点击事件（手动刷星界面（已废除））
+         * button1_KeyDown(object sender, KeyEventArgs e)--键盘监听事件（监听wasd改变蛇头位置）
+         * GG()--游戏结束与重新初始化事件
+         */
         private static Map map = new Map();
         private static People people = new People();
         private static Crah crah = new Crah();
-        private static Graphics backGround,boll,item;
+        private static Graphics backGround, boll, item;
         private static Timer timer = new Timer();
         private static Way.MoveWay way = Way.MoveWay.Down;
 
@@ -52,7 +68,7 @@ namespace GridDemo2
             {
                 case Way.MoveWay.Up:
                     boll.Clear(Color.White);
-                    people.MoveUp(crah,map);
+                    people.MoveUp(crah, map);
                     people.Updata(boll, map);
                     break;
                 case Way.MoveWay.Down:
@@ -80,7 +96,7 @@ namespace GridDemo2
         {
             backGround.Clear(Color.White);
             map.Create(backGround);
-            people.Updata(boll,map);
+            people.Updata(boll, map);
         }
 
         private void button1_KeyDown(object sender, KeyEventArgs e)
@@ -88,25 +104,25 @@ namespace GridDemo2
             switch (e.KeyCode)
             {
                 case Keys.W:
-                    if (way!=Way.MoveWay.Down)
+                    if (way != Way.MoveWay.Down)
                     {
                         way = Way.MoveWay.Up;
                     }
                     break;
                 case Keys.A:
-                    if (way!=Way.MoveWay.Right)
+                    if (way != Way.MoveWay.Right)
                     {
                         way = Way.MoveWay.Left;
                     }
                     break;
                 case Keys.S:
-                    if (way!=Way.MoveWay.Up)
+                    if (way != Way.MoveWay.Up)
                     {
                         way = Way.MoveWay.Down;
                     }
                     break;
                 case Keys.D:
-                    if (way!=Way.MoveWay.Left)
+                    if (way != Way.MoveWay.Left)
                     {
                         way = Way.MoveWay.Right;
                     }
