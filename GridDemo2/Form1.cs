67f5ec4053033bb13ca0097b22f3927c4b007719
neroaden <neroaden0515@outlook.com>
2018-04-18 18:36:51 +0800

    碰触死亡+选择运动反方向修正

--- GridDemo2/Form1.cs
+++ GridDemo2/Form1.cs
@@ -12,12 +12,12 @@ namespace GridDemo2
 {
     public partial class Form1 : Form
     {
-        Map map = new Map();
-        People people = new People();
-        Crah crah = new Crah();
-        Graphics backGround,boll,item;
-        Timer timer = new Timer();
-        Way.MoveWay way = Way.MoveWay.Down;
+        private static Map map = new Map();
+        private static People people = new People();
+        private static Crah crah = new Crah();
+        private static Graphics backGround,boll,item;
+        private static Timer timer = new Timer();
+        private static Way.MoveWay way = Way.MoveWay.Down;
 
         public Form1()
         {
@@ -27,14 +27,22 @@ namespace GridDemo2
             boll = CreateGraphics();
             timer.Interval = 500;
             timer.Tick += Timer_Tick;
+            init();
+        }
+
+        private static void init()
+        {
             map.x_max = 12;
             map.y_max = 12;
             map.space = 30;
             map.padding = 50;
             people.Location = new Point(1, 0);
             people.size = 8;
+            people.historyPoint.Clear();
             crah.cras_max = 2;
+            crah.cras.Clear();
             crah.AddCras(people.historyPoint, map);
+            way = Way.MoveWay.Down;
             timer.Start();
         }
 
@@ -80,20 +88,39 @@ namespace GridDemo2
             switch (e.KeyCode)
             {
                 case Keys.W:
-                    way = Way.MoveWay.Up;
+                    if (way!=Way.MoveWay.Down)
+                    {
+                        way = Way.MoveWay.Up;
+                    }
                     break;
                 case Keys.A:
-                    way = Way.MoveWay.Left;
+                    if (way!=Way.MoveWay.Right)
+                    {
+                        way = Way.MoveWay.Left;
+                    }
                     break;
                 case Keys.S:
-                    way = Way.MoveWay.Down;
+                    if (way!=Way.MoveWay.Up)
+                    {
+                        way = Way.MoveWay.Down;
+                    }
                     break;
                 case Keys.D:
-                    way = Way.MoveWay.Right;
+                    if (way!=Way.MoveWay.Left)
+                    {
+                        way = Way.MoveWay.Right;
+                    }
                     break;
                 default:
                     break;
             }
+        }
+
+        public static void GG()
+        {
+            timer.Stop();
+            MessageBox.Show("GG");
+            init();
         }
 
         private void Form1_Load(object sender, EventArgs e)
