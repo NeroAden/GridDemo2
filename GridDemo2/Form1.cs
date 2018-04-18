55375b7a9014798d4a7b62ff990b9ba686d713cd
neroaden <neroaden0515@outlook.com>
2018-04-18 19:15:10 +0800

    添加注释

--- GridDemo2/Form1.cs
+++ GridDemo2/Form1.cs
@@ -12,25 +12,53 @@ namespace GridDemo2
 {
     public partial class Form1 : Form
     {
-        Map map = new Map();
-        People people = new People();
-        Graphics backGround,boll;
-        Timer timer = new Timer();
-        Way.MoveWay way = Way.MoveWay.Down;
+        /*
+         * 初始化程序
+         * 实例化Map--背景网格
+         * People--贪吃蛇对象
+         * Crah--果子对象
+         * backGround--网格图层
+         * boll--贪吃蛇图层
+         * item--果子图层
+         * timer--总定时器
+         * way--蛇头朝向
+         * init()--初始化相关设置
+         * Timer_Tick(object sender, EventArgs e)--定时器定时事件（移动蛇以及相关逻辑）
+         * button1_Click(object sender, EventArgs e)--button1点击事件（手动刷星界面（已废除））
+         * button1_KeyDown(object sender, KeyEventArgs e)--键盘监听事件（监听wasd改变蛇头位置）
+         * GG()--游戏结束与重新初始化事件
+         */
+        private static Map map = new Map();
+        private static People people = new People();
+        private static Crah crah = new Crah();
+        private static Graphics backGround, boll, item;
+        private static Timer timer = new Timer();
+        private static Way.MoveWay way = Way.MoveWay.Down;
 
         public Form1()
         {
             InitializeComponent();
             backGround = CreateGraphics();
+            item = CreateGraphics();
             boll = CreateGraphics();
             timer.Interval = 500;
             timer.Tick += Timer_Tick;
+            init();
+        }
+
+        private static void init()
+        {
             map.x_max = 12;
             map.y_max = 12;
             map.space = 30;
             map.padding = 50;
             people.Location = new Point(1, 0);
             people.size = 8;
+            people.historyPoint.Clear();
+            crah.cras_max = 2;
+            crah.cras.Clear();
+            crah.AddCras(people.historyPoint, map);
+            way = Way.MoveWay.Down;
             timer.Start();
         }
 
@@ -40,34 +68,35 @@ namespace GridDemo2
             {
                 case Way.MoveWay.Up:
                     boll.Clear(Color.White);
-                    people.MoveUp(map);
+                    people.MoveUp(crah, map);
                     people.Updata(boll, map);
                     break;
                 case Way.MoveWay.Down:
                     boll.Clear(Color.White);
-                    people.MoveDown(map);
+                    people.MoveDown(crah, map);
                     people.Updata(boll, map);
                     break;
                 case Way.MoveWay.Left:
                     boll.Clear(Color.White);
-                    people.MoveLeft(map);
+                    people.MoveLeft(crah, map);
                     people.Updata(boll, map);
                     break;
                 case Way.MoveWay.Right:
                     boll.Clear(Color.White);
-                    people.MoveRight(map);
+                    people.MoveRight(crah, map);
                     people.Updata(boll, map);
                     break;
                 default:
                     break;
             }
+            crah.Draw(item, map);
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
             backGround.Clear(Color.White);
             map.Create(backGround);
-            people.Updata(boll,map);
+            people.Updata(boll, map);
         }
 
         private void button1_KeyDown(object sender, KeyEventArgs e)
@@ -75,20 +104,39 @@ namespace GridDemo2
             switch (e.KeyCode)
             {
                 case Keys.W:
-                    way = Way.MoveWay.Up;
+                    if (way != Way.MoveWay.Down)
+                    {
+                        way = Way.MoveWay.Up;
+                    }
                     break;
                 case Keys.A:
-                    way = Way.MoveWay.Left;
+                    if (way != Way.MoveWay.Right)
+                    {
+                        way = Way.MoveWay.Left;
+                    }
                     break;
                 case Keys.S:
-                    way = Way.MoveWay.Down;
+                    if (way != Way.MoveWay.Up)
+                    {
+                        way = Way.MoveWay.Down;
+                    }
                     break;
                 case Keys.D:
-                    way = Way.MoveWay.Right;
+                    if (way != Way.MoveWay.Left)
+                    {
+                        way = Way.MoveWay.Right;
+                    }
                     break;
                 default:
                     break;
             }
+        }
+
+        public static void GG()
+        {
+            timer.Stop();
+            MessageBox.Show("GG");
+            init();
         }
 
         private void Form1_Load(object sender, EventArgs e)
--- GridDemo2/People.cs
+++ GridDemo2/People.cs
@@ -9,51 +9,126 @@ namespace GridDemo2
 {
     class People
     {
+        /*
+         * Location--蛇头当前所在位置（默认起始位置）
+         * historyPoint--蛇所有身体的坐标列表
+         * size-列表大小，即蛇总长度
+         * 
+         * CheckCras(Point point,Crah cras,Map map)--确认蛇有没有吃到果子以及相关逻辑
+         * isMoveToBody(Point point)--确认蛇有没有碰到自己身体
+         * AutoList(Point point)--更新蛇的身体的坐标列表
+         * MoveUp(Crah crah, Map map)--蛇向上移动一格的实现
+         * MoveLeft(Crah crah, Map map)--蛇向左移动一格的实现
+         * MoveDown(Crah crah, Map map)--蛇向下移动一格的实现
+         * MoveRight(Crah crah, Map map)--蛇向右移动一格的实现
+         * Updata(Graphics g, Map map)--刷新相关界面
+         */
         public Point Location;
         public List<Point> historyPoint = new List<Point>();
         public int size;
+        private void CheckCras(Point point, Crah cras, Map map)
+        {
+            if (cras.cras.Contains(point))
+            {
+                size++;
+                cras.cras.Remove(point);
+                cras.AddCras(historyPoint, map);
+            }
+        }
+        private Boolean isMoveToBody(Point point)
+        {
+            if (historyPoint.Contains(point))
+            {
+                Form1.GG();
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
         private void AutoList(Point point)
         {
-            if (historyPoint.Count<size)
+            if (historyPoint.Count < size)
             {
                 historyPoint.Insert(0, point);
             }
             else
             {
-                historyPoint.RemoveAt(size-1);
+                historyPoint.RemoveAt(size - 1);
                 historyPoint.Insert(0, point);
             }
         }
-        public void MoveUp(Map map)
+        public void MoveUp(Crah crah, Map map)
         {
             if (Location.Y > 0)
             {
                 Location.Y--;
+                if (isMoveToBody(Location))
+                {
+                    Location.Y++;
+                    return;
+                }
+                CheckCras(Location, crah, map);
                 AutoList(Location);
             }
+            else
+            {
+                Form1.GG();
+            }
         }
-        public void MoveDown(Map map)
+        public void MoveDown(Crah crah, Map map)
         {
-            if (Location.Y < map.y_max-1)
+            if (Location.Y < map.y_max - 1)
             {
                 Location.Y++;
+                if (isMoveToBody(Location))
+                {
+                    Location.Y--;
+                    return;
+                }
+                CheckCras(Location, crah, map);
                 AutoList(Location);
             }
+            else
+            {
+                Form1.GG();
+            }
         }
-        public void MoveLeft(Map map)
+        public void MoveLeft(Crah crah, Map map)
         {
             if (Location.X > 0)
             {
                 Location.X--;
+                if (isMoveToBody(Location))
+                {
+                    Location.X++;
+                    return;
+                }
+                CheckCras(Location, crah, map);
                 AutoList(Location);
             }
+            else
+            {
+                Form1.GG();
+            }
         }
-        public void MoveRight(Map map)
+        public void MoveRight(Crah crah, Map map)
         {
-            if (Location.X < map.x_max-1)
+            if (Location.X < map.x_max - 1)
             {
                 Location.X++;
+                if (isMoveToBody(Location))
+                {
+                    Location.X--;
+                    return;
+                }
+                CheckCras(Location, crah, map);
                 AutoList(Location);
+            }
+            else
+            {
+                Form1.GG();
             }
         }
         public void Updata(Graphics g, Map map)
