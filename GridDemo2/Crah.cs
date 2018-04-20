367011592977e6cdc7e3eb34a5f20a07290a26ed
neroaden <neroaden0515@outlook.com>
2018-04-20 22:20:03 +0800

    Revert "添加注释"
    This reverts commit b69ec8956ed80edcbd35e931e4108b5555c28271.

--- GridDemo2/Crah.cs
+++ GridDemo2/Crah.cs
@@ -9,15 +9,9 @@ namespace GridDemo2
 {
     class Crah
     {
-        /*
-         * cras--所有果子的坐标列表
-         * cras_max--同时存在的果子的最大数
-         * AddCras(List<Point> body,Map map)--添加果子的实现
-         * Draw(Graphics g,Map map)--绘制（更新）果子图层
-         */
         public List<Point> cras = new List<Point>();
         public int cras_max;
-        public void AddCras(List<Point> body, Map map)
+        public void AddCras(List<Point> body,Map map)
         {
             Random random = new Random();
             while (cras.Count < 2)
@@ -29,7 +23,7 @@ namespace GridDemo2
                 }
             }
         }
-        public void Draw(Graphics g, Map map)
+        public void Draw(Graphics g,Map map)
         {
             foreach (var item in cras)
             {
